27620d3936967d529bb62ab38c9e76037161dde0
Kristofer <Kristofer@Kris>
2019-04-29 11:51:22 +0200

    Proper binary data and upload function

--- src/ftpclient/FTPClient.java
+++ src/ftpclient/FTPClient.java
@@ -1,27 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package ftpclient;
 
 import java.io.IOException;
 
+/**
+ *
+ * @author Nikolaj Landberg
+ */
 public class FTPClient {
 
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
         try {
+        //Connect to ftp server
         FTPIO FTP = new FTPIO("emby.waii.dk", "idunno", "47");
+        
+        //Send and print HELP
         String s1;
         s1 = FTP.send("HELP");
         System.out.println(s1);
         
+        //Initialize passive data connection (for LIST)
         var datasocket = FTP.initDataConnection();
         
-        //Vi viser indhold i rodmappen
+        //List root folder
         FTP.send("LIST");
-        
-        s1 = FTP.readFromDataSocket(datasocket, false);
+        byte[] bytes = FTP.readFromDataSocket(datasocket, false);
+        s1 = new String (bytes);
         System.out.println(s1);
         
+        //Get a file
         FTP.getFile("fil.txt");
         
+        //Get a file bigger than 1KB
+        FTP.getFile("NOR-gate.png");
         
+        //Create a file
+        String data = "Hello, how are you doing? :D";
+        byte[] content = data.getBytes();
+        FTP.createFile("NewFile.txt", content);
         
         }
         catch (IOException ex) {
--- src/ftpclient/FTPIO.java
+++ src/ftpclient/FTPIO.java
@@ -20,36 +20,38 @@ public FTPIO(String host, String user, String kode) throws IOException{
     System.out.println(s1);
     }
 
-//Implementer med binary-read hvis muligt..
 private String readLines(BufferedReader ind) throws IOException {
      String s1 = "";
      while (!ind.ready()) { } 
      while (true) {
         if (s1.isEmpty()) {
             s1 = ind.readLine(); 
-            if (s1.charAt(0) + s1.charAt(1) + s1.charAt(2) >= 100 && s1.charAt(3) != '-')
+            if (s1.length() >= 3 && Character.isDigit(s1.charAt(0)) && Character.isDigit(s1.charAt(1)) && Character.isDigit(s1.charAt(2)) && s1.charAt(3) != '-')
                 break;
         } else {
             String s2 = ind.readLine();
-            s1 = s1 + "\n" + s2;
-            if (s2.charAt(0) + s2.charAt(1) + s2.charAt(2) >= 100 && s2.charAt(3) != '-')
+            if (s2 == null)
+                break;
+            if (s2.isEmpty() || s2.length() >= 4 && s2.charAt(3) != '-') {
+                s1 = s1 + "\n";
+            }
+            s1 = s1 + s2;
+            if (s2.length() >= 4 && Character.isDigit(s2.charAt(0)) && Character.isDigit(s2.charAt(1)) && Character.isDigit(s2.charAt(2)) && s2.charAt(3) != '-')
                 break;
         }
     } 
     return s1;
 }
  
- public String send(String com) throws IOException {
+ public String send(String com) throws IOException{
      System.out.println("send: " + com);
      ud.println(com);
      ud.flush();
-     //Ryd op ---
      if(com.contains("LIST") || com.contains("RETR")) {
          String s1 = readLines(ind);
          s1 = s1 + readLines(ind);
          return s1;
      }
-     //---
      return readLines(ind);
 
  }
@@ -71,29 +73,53 @@ private String readLines(BufferedReader ind) throws IOException {
     return new Socket (IP, dataport);
  }
  
- public String readFromDataSocket(Socket datasocket, boolean printFirstKB) throws IOException {
-    var dataStream = new BufferedReader(new InputStreamReader(datasocket.getInputStream()));
-    String s1 = readLines(dataStream);
+ public byte[] readFromDataSocket(Socket dataSocket, boolean printFirstKB) throws IOException {
+    var dataStream = dataSocket.getInputStream();
+    final byte[] buffer = new byte[1];
+    int read = 1;
+    
+    ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+    
+    while ((read = dataStream.read(buffer)) > 0) {
+        byteStream.write(buffer);
+    }
+    
+    byte[] outBytes = byteStream.toByteArray();
     //Konverterer string til bytes og putter efterfølgende de første 1024 bytes ind i et andet bytearray og konverterer tilbage til string.
     if (printFirstKB) {
-        byte[] byteArray = s1.getBytes();
         byte[] firstKilobyte = new byte[1024];
         for (int i = 0; i < 1024; i++) {
-            if (i <= byteArray.length - 1) {
-                firstKilobyte[i] = byteArray[i];
+            if (i <= outBytes.length - 1) {
+                firstKilobyte[i] = outBytes[i];
             }
         }
         String s2 = new String (firstKilobyte);
-        System.out.println("First kilobyte: \n-------------------\n" + s2 + "\n-------------------\n");
+        System.out.println("First kilobyte: \n****-------------------****\n" + s2 + "\n****-------------------****\n");
     }
-    return s1;
+    
+    dataStream.close();
+    dataSocket.close();
+    
+    return outBytes;
  }
  
  public void getFile(String filename) throws IOException {
-     var datasocket = initDataConnection();
-     send("RETR " + filename);
-     String s1 = readFromDataSocket(datasocket, true);
-     //System.out.println(s1);
+    var datasocket = initDataConnection();
+    send("RETR " + filename);
+    byte[] readBytes = readFromDataSocket(datasocket, true);
      
+    try (FileOutputStream fos = new FileOutputStream(filename)) {
+        fos.write(readBytes);   
+    }
+ }
+ 
+ public void createFile(String filename, byte[] content) throws IOException {
+     var dataSocket = initDataConnection();
+     send("STOR " + filename);
+     PrintStream dataStream = new PrintStream(dataSocket.getOutputStream());
+     dataStream.write(content);
+     dataStream.close();
+     dataSocket.close();
+     System.out.println("File: " + filename + " delivered to server!");
  }
 }
